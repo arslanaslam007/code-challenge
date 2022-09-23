7add4ae1971f49c7379a8bf0c18fdf2ab35ceb26
Arslan Aslam <arslan.aslam@confiz.local>
2022-09-23 07:20:31 +0500

    code

---
+++ two-different-palindromes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin>>t;
+    
+    for(int i=1;i<=t;i++){
+        cin>>a>>b;
+    
+        // Both are Odd 
+        if (a%2 !=0 and b%2 != 0)
+             cout<<"No\n";
+        else if (a==1 or b==1)
+             cout<<"No\n";
+        else
+             cout<<"Yes\n";
+    }
+    
+    return 0;
+}
